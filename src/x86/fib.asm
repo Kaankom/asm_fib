fb24fb52b52cb4d1a29a034c7c2a6fe00d1fc57b
Kaan Komutekir <kaankom@hotmail.de>
2023-01-16 19:00:32 +0100

    Numbers get printed with libc printf

--- src/x86/fib.asm
+++ src/x86/fib.asm
@@ -1,41 +1,50 @@
 global _start
+extern printf
 
 section .data
     start_msg db "Fibonacci Sequence:", 0x0a
+    fib_num_format db "%d", 0x0a, 0x00
 
 section .text
 _start:
-    ; print start_message to stdout
+    call printMessage
+    call initFib
+    call fibLoop
+    call exit
+
+printMessage:
     mov rax, 1
     mov rdi, 1
     mov rsi, start_msg
     mov rdx, 20
     syscall
+    ret
 
+printNumber:
+    push rax
+    push rbx
+    mov rdi, fib_num_format
+    mov rsi, rbx
+    call printf
+    pop rbx
+    pop rax
+    ret
+
+initFib:
     xor rax, rax
     xor rbx, rbx
     inc rbx
+    ret
    
 fibLoop:
+    call printNumber
     add rax, rbx
     xchg rax, rbx
-
-    push rax
-    push rbx
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, [rsp+0x0a]
-    mov rdx, 1
-    syscall
-
-    pop rbx
-    pop rax
-
     cmp rbx, 10
     js fibLoop
+    ret
 
-    ; exit
+exit:
     mov rax, 60
     mov rdi, 0
     syscall
